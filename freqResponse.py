04ee52434a3f3ea6b88722be8d5eac33eaf220eb
Herbert Sauro <hsauro@u.washington.edu>
2023-07-19 12:57:47 -0700

    Add files via upload

--- freqResponse.py
+++ freqResponse.py
@@ -32,11 +32,101 @@ class FreqencyResponse:
         else:
            return math.atan2(val.imag, val.real)
 
+    # H_J (jw) = dv/ds H(jw) + dvdp
+    def getFluxFrequencyResponse(self, startFrequency, numberOfDecades, numberOfPoints,
+                      parameterId, variableId, useDB=True, useHz = True):
+        numReactions = self.r.getNumReactions()
+        numFloatingSpecies = self.r.getNumFloatingSpecies()
+        reactionIds = self.r.getReactionIds()
+        reactionIds = self.r.getReactionIds()
+        boundarySpeciesIds = self.r.getBoundarySpeciesIds()
+        globalParameterIds = self.r.getGlobalParameterIds()
+        
+        if variableId not in reactionIds:
+           raise Exception ("Can't find reaction:" + variableId + " in the model")     
+               
+        # Check that parameter name exists
+        if parameterId not in boundarySpeciesIds:
+           if parameterId not in globalParameterIds:
+               raise Exception ("Can't find parameter:" + variableName + " in the model")     
+       
+        try:
+           self.r.steadyState()
+        except Exception as e:
+            raise Exception ("Can't find the steady state: " + str (e))
+       
+        # Get the link matrix in case there are conserved moieties,
+        # equals the identity matrix if not
+        linkMatrix = self.r.getLinkMatrix()
+        # Convert Jacobian matrix to complex matrix
+        Jac = self.r.getFullJacobian()
+        Jac = np.array (Jac)
+        Jac = Jac.astype (complex)
+        # Compute Jac*linkMatrix
+        JacL = np.matmul (Jac, linkMatrix)
+        
+        # Will need elasticity matrix for the flux TFs 
+        ee = self.r.getUnscaledElasticityMatrix()
+        # Convert stoich matrix to complex matrix
+        Nr = self.r.getReducedStoichiometryMatrix()
+        Nr = np.array (Nr)
+        Nr = Nr.astype (complex)
+        
+        resultArray = np.zeros ((numberOfPoints, 3))
+    
+        identMatrix = np.identity(numFloatingSpecies, dtype=complex)
+        
+        w = self.getLogSpace (startFrequency, numberOfDecades, numberOfPoints)   
+        for i, wf in enumerate(w):
+            
+            # Compute first H(jw) = (jw I - Nr dv/dx L )^{-1} Nr dv/dp
+            jw_val = complex (0, wf)
+            T1 = identMatrix*jw_val
+            T2 = T1 - Jac
+            inverse = np.linalg.inv(T2)
+            T3 = np.matmul(inverse, Nr)
+            dvdp = np.zeros(numReactions, dtype=complex)
+            for k in range (numReactions): 
+                val = self.r.getUnscaledParameterElasticity(reactionIds[k], parameterId);
+                dvdp[k] = val+0j
+            T4 = np.matmul (T3, dvdp)       
+            
+            
+            # Next compute H^v (jw) = dv/ds H(jw) + dv/dp            
+            T4 = np.matmul (ee, T4) + dvdp
 
-    # Implement instead:
+            # we need to pick out the requested output flux
+            for  j in range (numReactions):
+                if (reactionIds[j] == variableId):
+                    dw = abs(T4[j])
+                    if useDB:
+                       dw = 20.0 * math.log10(dw)
+                    resultArray[i, 1] = dw
+        
+                    val = complex (T4[j], 0)
+                    phase = self.getPhase(val)
+                    resultArray[i, 2] = phase
+                    break
+    
+            if (useHz):
+               # Store frequency, convert to Hz by dividing by 2Pi
+               resultArray[i,0] = w[i] / (2. * math.pi);
+            else:
+               # Store frequency, leave as rad/sec
+               resultArray[i,0] = w[i]
+            
+        # This is to prevent discontinous jumps at the 180 degree points
+        resultArray[:,2] = np.unwrap(resultArray[:,2])
+        # Convert to degrees
+        resultArray[:,2] = (180/math.pi)*resultArray[:,2]
+        # Store result internally so that it can be used by plot
+        self.results = resultArray
+        return resultArray
+               
+    # Implement this:
     # This should be able to handle models with conserved cycles
     #   H(jw) = (jw I - Nr dv/dx L )^{-1} Nr dv/dp
-    def getFrequencyResponse(self, startFrequency, numberOfDecades, numberOfPoints,
+    def getSpeciesFrequencyResponse(self, startFrequency, numberOfDecades, numberOfPoints,
                       parameterId, variableId, useDB=True, useHz = True):
     
         numReactions = self.r.getNumReactions()
